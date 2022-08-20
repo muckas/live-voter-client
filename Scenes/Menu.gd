13e422780cbddc525d90db2bcd9b2815ac255997
muckas <motobike8@outlook.com>
2022-08-20 13:26:27 +0500

    Receiving images

--- Scenes/Menu.gd
+++ Scenes/Menu.gd
@@ -3,6 +3,8 @@ extends Control
 var server_base_url:String = "http://localhost:8080"
 
 onready var check_label = $CheckLabel
+onready var image_rect = $ImageRect
+
 
 func _on_CheckButton_pressed() -> void:
 	var endpoint:String = "/check"
@@ -19,3 +21,19 @@ func _on_check_completed(_result:int, _response_code:int, _headers:PoolStringArr
 			check_label.text = response["message"]
 	else:
 		check_label.text = "Server not responding"
+
+func _on_GetImageButton_pressed():
+	var endpoint:String = "/image"
+	var http:HTTPRequest = HTTPRequest.new()
+	self.add_child(http)
+	http.connect("request_completed", self, "_on_image_request_completed")
+	http.request(server_base_url + endpoint)
+
+func _on_image_request_completed(_result:int, _response_code:int, _headers:PoolStringArray, body:PoolByteArray) -> void:
+	var image:Image = Image.new()
+	var image_error:int = image.load_png_from_buffer(body)
+	if image_error != OK:
+		print("Image download failed")
+	var texture:ImageTexture = ImageTexture.new()
+	texture.create_from_image(image)
+	image_rect.texture = texture
